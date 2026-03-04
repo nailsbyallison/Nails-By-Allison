7c6167dde7f4a0e10b497d415f9cf53a68b4a236
nailsbyallison <nailsbyallison4@gmail.com>
2026-03-04 20:38:17 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,27 @@
-self.addEventListener('install', (event) => {
+const CACHE = "nba-v3";
+
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/manifest.webmanifest",
+  "/Rose%20Gold%20Black%20Elegant%20Luxury%20Circle%20Beauty%20Logo_20250608_164322_0000.png"
+];
+
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open('nba-v1').then((cache) => cache.addAll([
-      './',
-      './index.html',
-      './Logo.png'
-    ]))
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((cached) => cached || fetch(event.request))
   );
